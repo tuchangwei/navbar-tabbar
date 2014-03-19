882c8ac6c6213a581e5a8f3ca1c11a02d3ee9f97
Changwei Tu <changweitu@gmail.com>
2014-03-19 09:19:31 +0800

    Initial Commit

---
+++ CustomTabbarTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CustomTabbarTest
+//
+//  Created by Leelen-mac1 on 14-3-19.
+//  Copyright (c) 2014年 Leelen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
